16985ed1ab07ec0000a410e0eb3d832ba340fbec
Zsolt_Sebestyen <zsolt.sebestyen@fornax.hu>
2023-06-25 13:00:12 +0200

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
-  output: "server",
-  adapter: cloudflare()
+	integrations: [
+		tailwind(),
+		mdx(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+	],
 });
