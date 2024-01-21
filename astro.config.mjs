ae4703a20969fe2eba86cf1491a0cf2115534c52
Zsolt_Sebestyen <zsolt.sebestyen@fornax.hu>
2024-01-21 19:32:30 +0100

    upgrade to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		mdx(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-	],
+  integrations: [tailwind(), mdx(), icon()]
 });
