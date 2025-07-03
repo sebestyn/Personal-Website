07af1a6fce5a24693e76ba86eed05a38ace06e49
sebestyn <sebestyn.zsolt@gmail.com>
2025-07-03 10:38:57 +0200

    refactor: update CV link, enhance project layout, and improve card styles.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,18 @@ module.exports = {
 			white: '#fff',
 			orange: '#E06330',
 			orangeDark: '#D05A2D',
+			gray: {
+				50: '#f9fafb',
+				100: '#f3f4f6',
+				200: '#e5e7eb',
+				300: '#d1d5db',
+				400: '#9ca3af',
+				500: '#6b7280',
+				600: '#4b5563',
+				700: '#374151',
+				800: '#1f2937',
+				900: '#111827',
+			},
 		},
 		fontFamily: {
 			// Headings
